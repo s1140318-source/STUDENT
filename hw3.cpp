2b51e5171a1b99bae6e1c6bad6c8886ea358c138
s1140318-source <s1140318@dmhs.kh.edu.tw>
2025-10-23 10:00:42 +0800

    Add files via upload

---
+++ hw3.cpp
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout <<  "1. 快樂星球"  << endl;
+    cout <<  "2. 快樂星球"  << endl;
+    cout <<  "3. 快樂星球"  << endl;
+    cout <<  "4. 快樂星球"  << endl;
+    cout <<  "5. 快樂星球"  << endl;
+    cout <<  "6. 快樂星球"  << endl;
+    cout <<  "7. 快樂星球"  << endl;
+    
+    return 0;
+}
