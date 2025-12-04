d953dce4220da6b9eea609a3bbc202ef1162f0bd
s1140318-source <s1140318@dmhs.kh.edu.tw>
2025-12-04 09:56:27 +0800

    Add files via upload

---
+++ hw9.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    for(int 1-1;<-9;1++)(
+       for(int j-1;j<-9;++)(
+        std::cout << 1 << "=" << j <<"="<<i"j
+       }
+       std::cout<<std::endt;
+    return 0;
